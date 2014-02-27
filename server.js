673e7266ff6ecbfbc22da6d55713dfbb2d258244
Silas Baronda <silas.baronda@gmail.com>
2014-02-27 07:18:46 -0500

    Using Express for routing and Jade for templates.

--- server.js
+++ server.js
@@ -5,92 +5,78 @@ var http = require('http'),
     // This is our React component, shared by server and browser thanks to browserify
     MyApp = require('./myApp')
 
+var express = require('express');
+var app = express();
 
-// Just create a plain old HTTP server that responds to two endpoints ('/' and
-// '/bundle.js') This would obviously work similarly with any higher level
-// library (Express, etc)
-http.createServer(function(req, res) {
+var pub = __dirname + '/public';
+app.use(express.static(pub));
 
-  // If we hit the homepage, then we want to serve up some HTML - including the
-  // server-side rendered React component(s), as well as the script tags
-  // pointing to the client-side code
-  if (req.url == '/') {
+app.set('view engine', 'jade');
 
-    // This represents our data to be passed in to the React component for
-    // rendering - just as you would pass data, or expose variables in
-    // templates such as Jade or Handlebars.  We just use an array of garbage
-    // here (with some potentially dangerous values for testing), but you could
-    // imagine this would be objects typically fetched async from a DB,
-    // filesystem or API, depending on the logged-in user, etc.
-    var props = {items: [0, 1, '</script>', '<!--inject!-->']}
+app.set('views', __dirname + '/views');
 
-    // Now that we've got our data, we can perform the server-side rendering by
-    // passing it in as `props` to our React component - and returning an HTML
-    // string to be sent to the browser
-    var myAppHtml = React.renderComponentToString(MyApp(props))
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.send(500, 'Something broke!');
+});
 
-    res.setHeader('Content-Type', 'text/html')
+app.get('/', function(req, res) {
+   var props = {items: [0, 1, '</script>', '<!--inject!-->']}
 
-    // Now send our page content - this could obviously be constructed in
-    // another template engine, or even as a top-level React component itself -
-    // but easier here just to construct on the fly
-    res.end(
-      // <html>, <head> and <body> are for wusses
+   var myAppHtml = React.renderComponentToString(MyApp(props))
 
-      // Include our static React-rendered HTML in our content div. This is
-      // the same div we render the component to on the client side, and by
-      // using the same initial data, we can ensure that the contents are the
-      // same (React is smart enough to ensure no rendering will actually occur
-      // on page load)
-      '<div id=content>' + myAppHtml + '</div>' +
+   res.setHeader('Content-Type', 'text/html')
 
-      // We'll load React from a CDN - you don't have to do this,
-      // you can bundle it up or serve it locally if you like
-      '<script src=//fb.me/react-0.9.0.min.js></script>' +
+   res.render('index', {myAppHtml: {test: myAppHtml, props: safeStringify(props)}})
+   // res.end(
+   //    // <html>, <head> and <body> are for wusses
 
-      // Then the browser will fetch the browserified bundle, which we serve
-      // from the endpoint further down. This exposes our component so it can be
-      // referenced from the next script block
-      '<script src=/bundle.js></script>' +
+   //    // Include our static React-rendered HTML in our content div. This is
+   //    // the same div we render the component to on the client side, and by
+   //    // using the same initial data, we can ensure that the contents are the
+   //    // same (React is smart enough to ensure no rendering will actually occur
+   //    // on page load)
+   //    '<div id=content>' + myAppHtml + '</div>' +
 
-      // This script renders the component in the browser, referencing it
-      // from the browserified bundle, using the same props we used to render
-      // server-side. We could have used a window-level variable, or even a
-      // JSON-typed script tag, but this option is safe from namespacing and
-      // injection issues, and doesn't require parsing
-      '<script>' +
-        'var MyApp = require("./myApp.js"), container = document.getElementById("content");' +
-        'React.renderComponent(MyApp(' + safeStringify(props) + '), container)' +
-      '</script>'
-    )
+   //    // We'll load React from a CDN - you don't have to do this,
+   //    // you can bundle it up or serve it locally if you like
+   //    '<script src=//fb.me/react-0.9.0.min.js></script>' +
 
-  // This endpoint is hit when the browser is requesting bundle.js from the page above
-  } else if (req.url == '/bundle.js') {
+   //    // Then the browser will fetch the browserified bundle, which we serve
+   //    // from the endpoint further down. This exposes our component so it can be
+   //    // referenced from the next script block
+   //    '<script src=/bundle.js></script>' +
 
-    res.setHeader('Content-Type', 'text/javascript')
+   //    // This script renders the component in the browser, referencing it
+   //    // from the browserified bundle, using the same props we used to render
+   //    // server-side. We could have used a window-level variable, or even a
+   //    // JSON-typed script tag, but this option is safe from namespacing and
+   //    // injection issues, and doesn't require parsing
+   //    '<script>' +
+   //      'var MyApp = require("./myApp.js"), container = document.getElementById("content");' +
+   //      'React.renderComponent(MyApp(' + safeStringify(props) + '), container)' +
+   //    '</script>'
+   //  )
+})
 
-    // Here we invoke browserify to package up our component.
-    // DON'T do it on the fly like this in production - it's very costly -
-    // either compile the bundle ahead of time, or use some smarter middleware
-    // (eg browserify-middleware).
-    // We also use literalify to transform our `require` statements for React
-    // so that it uses the global variable (from the CDN JS file) instead of
-    // bundling it up with everything else
-    browserify()
-      .transform(literalify.configure({react: 'window.React'}))
-      .require('./myApp.js')
-      .bundle()
-      .pipe(res)
+app.get('/bundle.js', function(req, res) {
+  res.setHeader('Content-Type', 'text/javascript')
 
-  // Return 404 for all other requests
-  } else {
-    res.statusCode = 404
-    res.end()
-  }
+  // Here we invoke browserify to package up our component.
+  // DON'T do it on the fly like this in production - it's very costly -
+  // either compile the bundle ahead of time, or use some smarter middleware
+  // (eg browserify-middleware).
+  // We also use literalify to transform our `require` statements for React
+  // so that it uses the global variable (from the CDN JS file) instead of
+  // bundling it up with everything else
+  browserify()
+    .transform(literalify.configure({react: 'window.React'}))
+    .require('./myApp.js')
+    .bundle()
+    .pipe(res)
+})
 
-// The http server listens on port 3000
-}).listen(3000)
-
+app.listen(3000)
 
 // A utility function to safely escape JSON for embedding in a <script> tag
 function safeStringify(obj) {
